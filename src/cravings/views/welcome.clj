7582064aa2f6ec566c44ea562f8ead13d9a97ac1
Tom O'Brien <thomas.obrien86@gmail.com>
2012-05-26 15:53:40 +0100

    Talking to mongo

--- src/cravings/views/welcome.clj
+++ src/cravings/views/welcome.clj
@@ -1,10 +1,12 @@
 (ns cravings.views.welcome
   (:require [cravings.views.common :as common]
             [noir.content.getting-started]
-            [monger.core :as mg])
+            [monger.core :as mg]
+            [monger.collection :as mc])
   (:use [noir.core :only [defpage]]))
 
-
+(def mongo-uri "mongodb://127.0.0.1/")
+(def config { :uri "mongodb://127.0.0.1", :db "cravings"})
 
 (defpage "/welcome" []
          (common/layout
