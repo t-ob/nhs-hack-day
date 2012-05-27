1f9673e25d04a8aebe62231cd868dc2352e4bca6
Tom O'Brien <thomas.obrien86@gmail.com>
2012-05-27 10:05:06 +0100

    Getting there

--- src/cravings/views/welcome.clj
+++ src/cravings/views/welcome.clj
@@ -4,20 +4,24 @@
             [monger.core :as mg]
             [monger.collection :as mc]
             [monger.query :as mq])
-  (:use [noir.core :only [defpage]]))
+  (:use [noir.core :only [defpage]]
+        monger.operators)
+  (:import [org.bson.types BSONTimestamp]))
 
 (def mongo-uri "mongodb://127.0.0.1/")
-(def config { :uri "mongodb://127.0.0.1/cravings",
-             :db "cravings",
-             :strats "strats"})
+(def config {:uri "mongodb://127.0.0.1/cravings"
+             :users "users"
+             :strats "strats"
+             :cravings "cravings"})
 
 (mg/connect-via-uri! (:uri config))
 
 (defn make-user
   "Add a user"
-  [user-name first-name last-name age]
-  (mc/insert "cravings"
+  [user-name e-mail first-name last-name age]
+  (mc/insert (:users config)
              {:user user-name
+              :mail e-mail
               :first first-name
               :last last-name
               :age age
@@ -26,17 +30,34 @@
 (defn make-strat
   "Add a strategy to database"
   [strategy]
-  (mc/insert "strats" { :content strategy }))
+  (mc/insert (:strats config) { :content strategy }))
+
+(defn make-craving
+  "Generate a craving object"
+  [user-name]
+  (mc/insert (:cravings config) {:timestamp (BSONTimestamp.)
+                                 :user user-name
+                                 :result 0}))
 
 (defn get-random-record
   "Get a random record"
   [coll]
   (nth coll (rand-int (count coll))))
 
+(defn update-score [user amount]
+  (mc/update (:users config)
+             {:user (:user user)}
+             {"$inc" {:points amount}}))
 
+(def user
+  (first (mc/find-maps "users" {:user "tobrien"})))
 
 (defpage "/" []
-  "helloooo")
+  (common/layout
+   [:h1 "Cravings app"]
+   [:p (str "Tom O'Brien - " (:points user) " points.")]
+   [:p
+    [:a "I have a craving"]]))
 
 (defpage [:post "/login"] {:keys [username password]}
   (str "You tried to login as " username " with the password " password))
@@ -50,4 +71,4 @@
 (defpage "/welcome" []
          (common/layout
            [:p "Welcome to cravings"]
-           [:p (str "There are " (mc/count "cravings") " users.")]))
+           [:p (str "There are " (mc/count "users") " users.")]))
